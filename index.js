20887d2bceadc724503a08e90c25239291a78c41
EdsonJSF <edsonjsf1990@gmail.com>
2023-06-22 07:37:40 -0400

    Installing node Express

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("Hola mundo");
+const express = require("express");
+const app = express();
+
+const port = 8000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.get("*", (req, res) => {
+  res.send("404 | Page not found");
+});
+
+app.listen(port, () => {
+  console.log(`App listening at:`);
+  console.log(`http://localhost:${port}`);
+});
