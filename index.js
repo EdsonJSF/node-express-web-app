b4b202a6d5b9de7d859d9ba3b3fd5180c8ca4c01
EdsonJSF <edsonjsf1990@gmail.com>
2023-06-27 08:47:12 -0400

    Using partials with HBS

--- index.js
+++ index.js
@@ -1,20 +1,39 @@
 const express = require("express");
-const app = express();
+const hbs = require("hbs");
 
+const app = express();
 const port = 8000;
 
 // Handlebars
 app.set("view engine", "hbs");
+hbs.registerPartials(__dirname + "/views/partials", (err) => {});
 
 // Show static content
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.render("home");
+  res.render("home", {
+    title: "Home",
+    name: "Edson Sánchez",
+  });
+});
+
+app.get("/elements", (req, res) => {
+  res.render("elements", {
+    title: "Elements",
+    name: "Edson Sánchez",
+  });
+});
+
+app.get("/generic", (req, res) => {
+  res.render("generic", {
+    title: "Generic",
+    name: "Edson Sánchez",
+  });
 });
 
 app.get("*", (req, res) => {
-  res.sendFile(__dirname + "/public/404.html");
+  res.render("404");
 });
 
 app.listen(port, () => {
